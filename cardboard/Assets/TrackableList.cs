ed6873759e1f1730c0cc883f90dee3a9eeb41a76
Ivon Liu <me@ivonliu.com>
2016-11-13 00:55:46 -0800

    Vuforia

---
+++ cardboard/Assets/TrackableList.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Vuforia;
+
+public class TrackableList : MonoBehaviour {
+
+	void Start () {
+		Debug.Log ("This is working!");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log ("Starting something hello world!!");
+		// Get the Vuforia StateManager
+		StateManager sm = TrackerManager.Instance.GetStateManager ();
+
+		// Query the StateManager to retrieve the list of
+		// currently 'active' trackables 
+		//(i.e. the ones currently being tracked by Vuforia)
+		IEnumerable<TrackableBehaviour> activeTrackables = sm.GetActiveTrackableBehaviours ();
+
+		// Iterate through the list of active trackables
+		//Debug.Log ("List of trackables currently active (tracked): ");
+		foreach (TrackableBehaviour tb in activeTrackables) {
+			Debug.Log("Trackable!!!!!!!!: " + tb.TrackableName);
+		}
+	}
+}
